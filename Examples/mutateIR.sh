d124accb8de7729fdd6f96e8005b2fbe92ec0318
Farah Hariri <hariri2@illinois.edu>
2018-05-24 16:49:28 -0500

    fixing paths in mutationIR.sh script

--- Examples/mutateIR.sh
+++ Examples/mutateIR.sh
@@ -5,7 +5,7 @@ CURR_DIR=`pwd`
 rm -f /tmp/llvm_mutate_trace # remove any existing coverage
 rm -rf ~/.srciror # remove logs and results from previous runs
 echo "SRCIROR: Instrumenting for Coverage"
-SRCIROR_COVINSTRUMENTATION_LIB=$CURR_DIR/../IRMutation/InstrumentationLib/SRCIRORCoverageLib.o SRCIROR_LLVMMutate_LIB=/scratch/hariri2/c-optimute/experiment/tools/llvm-build/Release+Asserts/lib/LLVMMutate.so SRCIROR_LLVM_BIN=/scratch/hariri2/c-optimute/experiment/tools/llvm-build/Release+Asserts/bin/ python $CURR_DIR/../IRMutation/PythonWrappers/irCoverageClang test.c -o test
+SRCIROR_COVINSTRUMENTATION_LIB=$CURR_DIR/../IRMutation/InstrumentationLib/SRCIRORCoverageLib.o SRCIROR_LLVMMutate_LIB=`pwd`/../tools/llvm-build/Release+Asserts/lib/LLVMMutate.so SRCIROR_LLVM_BIN=`pwd`/../tools/llvm-build/Release+Asserts/bin/ python $CURR_DIR/../PythonWrappers/irCoverageClang test.c -o test
 # run the executable to collect coverage
 ./test
 echo "the collected coverage is under /tmp/llvm_coverage"
@@ -13,7 +13,7 @@ echo "the collected coverage is under /tmp/llvm_coverage"
 
 # generate mutation opportunities
 echo "SRCIROR: generating mutation opportunities"
-SRCIROR_COVINSTRUMENTATION_LIB=$CURR_DIR/../IRMutation/InstrumentationLib/SRCIRORCoverageLib.o SRCIROR_LLVMMutate_LIB=/scratch/hariri2/c-optimute/experiment/tools/llvm-build/Release+Asserts/lib/LLVMMutate.so SRCIROR_LLVM_BIN=/scratch/hariri2/c-optimute/experiment/tools/llvm-build/Release+Asserts/bin/ python $CURR_DIR/../IRMutation/PythonWrappers/irMutationClang test.c
+SRCIROR_COVINSTRUMENTATION_LIB=$CURR_DIR/../IRMutation/InstrumentationLib/SRCIRORCoverageLib.o SRCIROR_LLVMMutate_LIB=`pwd`/../tools/llvm-build/Release+Asserts/lib/LLVMMutate.so SRCIROR_LLVM_BIN=`pwd`/../tools/llvm-build/Release+Asserts/bin/ python $CURR_DIR/../PythonWrappers/irMutationClang test.c
 echo "The generated mutants are under ~/.srciror/bc-mutants/681837891"
 
 # TODO: intersect with coverage
@@ -22,4 +22,4 @@ file_name=`cat ~/.srciror/ir-coverage/hash-map | grep "test.c" | cut -f1 -d:`
 mutation=`head -n1 ~/.srciror/bc-mutants/681837891 | cut -f1 -d,`
 echo "file name is: $file_name and mutation requested is: $mutation" 
 echo "$file_name:$mutation" > ~/.srciror/mutation_request.txt
-SRCIROR_COVINSTRUMENTATION_LIB=$CURR_DIR/../IRMutation/InstrumentationLib/SRCIRORCoverageLib.o SRCIROR_LLVMMutate_LIB=/scratch/hariri2/c-optimute/experiment/tools/llvm-build/Release+Asserts/lib/LLVMMutate.so SRCIROR_LLVM_BIN=/scratch/hariri2/c-optimute/experiment/tools/llvm-build/Release+Asserts/bin/ python $CURR_DIR/../IRMutation/PythonWrappers/irVanillaClang test.c
+SRCIROR_COVINSTRUMENTATION_LIB=$CURR_DIR/../IRMutation/InstrumentationLib/SRCIRORCoverageLib.o SRCIROR_LLVMMutate_LIB=`pwd`/../tools/llvm-build/Release+Asserts/lib/LLVMMutate.so SRCIROR_LLVM_BIN=`pwd`/../tools/llvm-build/Release+Asserts/bin/ python $CURR_DIR/../PythonWrappers/irVanillaClang test.c
