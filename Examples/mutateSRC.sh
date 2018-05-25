f6c715f49c3a62bad5714d387c563e24ab89f9a1
August Shi <awshi2@illinois.edu>
2018-05-25 18:48:52 -0500

    Doing the same for src

--- Examples/mutateSRC.sh
+++ Examples/mutateSRC.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # generate all src mutants (assuming all lines are covered)
-CURR_DIR=`pwd`
+CURR_DIR=$( cd $( dirname $0 ) && pwd )
 
 rm -rf ~/.srciror
 mkdir ~/.srciror
-echo "`pwd`/test.c:3,4,5,6,7,8,9" > ~/.srciror/coverage
+echo "$CURR_DIR/test.c:3,4,5,6,7,8,9" > ~/.srciror/coverage
 
-SRCIROR_LLVM_BIN=`pwd`/../tools/llvm-build/Release+Asserts/bin SRCIROR_LLVM_INCLUDES=`pwd`/../tools/llvm-build/Release+Asserts/lib/clang/3.8.0/include SRCIROR_SRC_MUTATOR=`pwd`/../SRCMutation/build/mutator python $CURR_DIR/../PythonWrappers/mutationClang `pwd`/test.c -o test
-
+SRCIROR_LLVM_BIN=$CURR_DIR/../tools/llvm-build/Release+Asserts/bin
+SRCIROR_LLVM_INCLUDES=$CURR_DIR/../tools/llvm-build/Release+Asserts/lib/clang/3.8.0/include
+SRCIROR_SRC_MUTATOR=$CURR_DIR/../SRCMutation/build/mutator
+python $CURR_DIR/../PythonWrappers/mutationClang $CURR_DIR/test.c -o test
