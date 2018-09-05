98686594f70518e4a0847b88bc86d445e9618796
Thomas Lemberger <lembergerth@gmail.com>
2018-09-05 21:32:38 +0200

    Make mutateSRC useable.
    Fixes wrong paths and adds export-statements that are necessary (on my
    system).
    
    mutateIR is still not working because it is using the `../tools` directory
    instead of just `../`, somewhere.

--- Examples/mutateSRC.sh
+++ Examples/mutateSRC.sh
@@ -7,7 +7,7 @@ rm -rf ~/.srciror
 mkdir ~/.srciror
 echo "$CURR_DIR/test.c:3,4,5,6,7,8,9" > ~/.srciror/coverage
 
-SRCIROR_LLVM_BIN=$CURR_DIR/../tools/llvm-build/Release+Asserts/bin
-SRCIROR_LLVM_INCLUDES=$CURR_DIR/../tools/llvm-build/Release+Asserts/lib/clang/3.8.0/include
-SRCIROR_SRC_MUTATOR=$CURR_DIR/../SRCMutation/build/mutator
+export SRCIROR_LLVM_BIN=$CURR_DIR/../llvm-build/Release+Asserts/bin
+export SRCIROR_LLVM_INCLUDES=$CURR_DIR/../llvm-build/Release+Asserts/lib/clang/3.8.0/include
+export SRCIROR_SRC_MUTATOR=$CURR_DIR/../SRCMutation/build/mutator
 python $CURR_DIR/../PythonWrappers/mutationClang $CURR_DIR/test.c -o test
